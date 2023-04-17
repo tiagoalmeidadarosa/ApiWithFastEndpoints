7c19f34bf009a79834840d6c1764d7b537b07414
Tiago Almeida da Rosa <tiago.almeida@poatek.com>
2023-04-17 17:06:14 -0300

    Adding Swagger based on documentation

--- ApiWithFastEndpoints/Program.cs
+++ ApiWithFastEndpoints/Program.cs
@@ -9,10 +9,8 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
 builder.Services.AddSwaggerDoc();
-
 builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
     new SqliteConnectionFactory(connectionString: builder.Configuration.GetConnectionString("Database")!));
-
 builder.Services.AddTransient<MovieService>();
 
 var app = builder.Build();
@@ -22,9 +20,7 @@ app.UseFastEndpoints(c =>
     c.Endpoints.RoutePrefix = "api";
     c.Serializer.Options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
-
-app.UseOpenApi();
-app.UseSwaggerUi3(s => s.ConfigureDefaults());
+app.UseSwaggerGen();
 
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
